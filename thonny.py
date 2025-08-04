2f47840186fb695514cc1b1f8e39cb73a68bc364
Melisa Cihan <melisa-cihan@outlook.de>
2025-08-04 13:46:22 +0200

    implement callback function + refactor code

--- thonny.py
+++ thonny.py
@@ -17,36 +17,16 @@ password = 'rnFIW625'
 mqtt_server = '10.10.4.58'
 mqtt_user = 'pi'
 mqtt_pass = '1Himbeere'
-mqtt_port = 1883
 
 #MQTT SetUp
 client_id = ubinascii.hexlify(machine.unique_id())
+topic_sub = b'notification'
 topic_pub = b'sensors/inside/temperature'
 
-# Sensor setup example
-sensor = dht.DHT22(machine.Pin(4))
-
-# Connect to MQTT Broker
-def connect_mqtt():
-    client = MQTTClient(client_id, mqtt_server, user=mqtt_user, password=mqtt_pass)
-    client.connect()
-    print('Connected to MQTT Broker')
-    return client
-
-def reconnect():
-    print('Failed to connect to MQTT broker. Reconnecting...')
-    time.sleep(5)
-    machine.reset()
-
-try:
-    client = connect_mqtt()
-except OSError as e:
-    reconnect()
-    
-topic_sub = b'hello'
 last_message = 0
 message_interval = 5
 counter = 0
+   
 
 # Establish Wifi Connection
 station = network.WLAN(network.STA_IF)
@@ -57,5 +37,40 @@ while station.isconnected() == False:
 print('Connection successful')
 print(station.ifconfig)
 
+# Connect and Reconnect Functions
+def connect_and_subscribe():
+    global client_id, mqtt_server, topic_sub
+    client = MQTTClient(client_id, mqtt_server, user=mqtt_user, password=mqtt_pass)
+    client.set_callback(sub_cb)
+    client.connect()
+    client.subscribe(topic_sub)
+    print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+    return client
 
+def restart_and_reconnect():
+    print('Failed to connect to MQTT broker.
+    Reconnecting...')
+    time.sleep(10)
+    machine.reset()
+
+# Callback Funnction and Main Loop
+def sub_cb(topic, msg):
+    print((topic, msg))
+    if topic == b'notification' and msg == b'received':
+    print('ESP received hello message')
+    try:
+        client = connect_and_subscribe()
+    except OSError as e:
+        restart_and_reconnect()
+    while True:
+        try:
+            client.check_msg()
+            if (time.time() - last_message) > message_interval:
+                msg = b'Hello #%d' % counter
+                client.publish(topic_pub, msg)
+                last_message = time.time()
+                counter += 1
+            except OSError as e:
+                restart_and_reconnect()
+    
 
