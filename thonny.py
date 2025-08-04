e50947ebbadd4ee721848e0a2fe4644a9ecb23e0
Melisa Cihan <melisa-cihan@outlook.de>
2025-08-04 13:27:42 +0200

    implement mqtt connection

--- thonny.py
+++ thonny.py
@@ -10,16 +10,38 @@ esp.osdebug(None)
 import gc
 gc.collect()
 
-# Constants
+# MQTT Broker Info Constants
 ssid = 'Rechnernetze'
 password = 'rnFIW625'
 mqtt_server = '10.10.4.58'
 mqtt_user = 'pi'
 mqtt_pass = '1Himbeere'
 
+#MQTT SetUp
 client_id = ubinascii.hexlify(machine.unique_id())
-topic_sub = b'notification'
-topic_pub = b'hello'
+topic_pub = b'sensors/inside/temperature'
+
+# Sensor setup example
+sensor = dht.DHT22(machine.Pin(4))
+
+# Connect to MQTT Broker
+def connect_mqtt():
+    client = MQTTClient(client_id, mqtt_server, user=mqtt_user, password=mqtt_pass)
+    client.connect()
+    print('Connected to MQTT Broker')
+    return client
+
+def reconnect():
+    print('Failed to connect to MQTT broker. Reconnecting...')
+    time.sleep(5)
+    machine.reset()
+
+try:
+    client = connect_mqtt()
+except OSError as e:
+    reconnect()
+    
+topic_sub = b'hello'
 last_message = 0
 message_interval = 5
 counter = 0
